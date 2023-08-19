5511ef73e9076c1ddc3d28d7d3d03f766fd01984
Yogendra Jaiswal <yogendra.jaiswal.503@gmail.com>
2023-08-20 02:12:24 +0530

    add: feat to read from std in if file name is empty

--- word-count/main.go
+++ word-count/main.go
@@ -13,32 +13,62 @@ func main() {
 }
 
 func openFile(filePath *string) (*bufio.Reader, error) {
+	if *filePath == "" {
+		return bufio.NewReader(os.Stdin), nil
+	}
 	fileStream, err := os.Open(*filePath)
 	if err != nil {
-		return nil, errors.New("FILE NOT FOUND")
+		return nil, errors.New("FILE READ ERROR")
 	}
 	return bufio.NewReaderSize(fileStream, 4096), nil
 }
 
 func getFormattedOutput(args []string) string {
-	filepath := args[0]
+	wordsFlag, linesFlag, bytesFlag, charsFlag, filepath, err := parseArgs(args)
+	if err != nil {
+		return "INVALID ARGUMENT"
+	}
 	reader, err := openFile(&filepath)
 	if err != nil {
-		return fmt.Sprintf("0 0 0 %s", filepath)
+		return err.Error()
 	}
-	wordsCount, linesCount, bytesCount, _ := getCounts(reader)
+	wordsCount, linesCount, bytesCount, charsCount, err := getCounts(reader)
 
-	return fmt.Sprintf("%d %d %d %s", linesCount, wordsCount, bytesCount, filepath)
+	if err != nil {
+		return err.Error()
+	}
+
+	output := ""
+
+	if linesFlag {
+		output += fmt.Sprintf("%d ", linesCount)
+	}
+	if wordsFlag {
+		output += fmt.Sprintf("%d ", wordsCount)
+	}
+	if bytesFlag {
+		output += fmt.Sprintf("%d ", bytesCount)
+	}
+	if charsFlag {
+		output += fmt.Sprintf("%d ", charsCount)
+	}
+	if !(linesFlag || wordsFlag || bytesFlag || charsFlag) {
+		output += fmt.Sprintf("%d %d %d ", linesCount, wordsCount, bytesCount)
+	}
+	if filepath != "" {
+		output += filepath
+	}
+	return output
 }
 
-func getCounts(reader *bufio.Reader) (int, int, int, int) {
+func getCounts(reader *bufio.Reader) (int, int, int, int, error) {
 	wordsCount, linesCount, bytesCount, charsCount := 0, 0, 0, 0
 
 	for {
 		line, err := reader.ReadBytes('\n')
 		if err != nil {
 			if err.Error() != "EOF" {
-				return 0, 0, 0, 0
+				return 0, 0, 0, 0, errors.New("FILE READ ERROR")
 			}
 			break
 		}
@@ -47,5 +77,38 @@ func getCounts(reader *bufio.Reader) (int, int, int, int) {
 		charsCount += len(bytes.Runes(line))
 		wordsCount += len(bytes.Fields(line))
 	}
-	return wordsCount, linesCount, bytesCount, charsCount
+	return wordsCount, linesCount, bytesCount, charsCount, nil
+}
+
+func parseArgs(args []string) (bool, bool, bool, bool, string, error) {
+	wordsFlag, linesFlag, bytesFlag, charsFlag := false, false, false, false
+	filepath := ""
+
+	for _, arg := range args {
+		if isFlag(arg) {
+			for _, flag := range arg[1:] {
+				switch flag {
+				case 'w':
+					wordsFlag = true
+				case 'l':
+					linesFlag = true
+				case 'c':
+					bytesFlag = true
+				case 'm':
+					charsFlag = true
+				}
+			}
+		} else {
+			if filepath == "" {
+				filepath = arg
+			} else if arg != "" {
+				return false, false, false, false, "", errors.New("INVALID ARGUMENT")
+			}
+		}
+	}
+	return wordsFlag, linesFlag, bytesFlag, charsFlag, filepath, nil
+}
+
+func isFlag(arg string) bool {
+	return arg[0] == '-'
 }
--- word-count/main_test.go
+++ word-count/main_test.go
@@ -15,8 +15,44 @@ func TestFormattedOutput_NoFlags_ValidFile(t *testing.T) {
 
 func TestFormattedOutput_NoFlags_InvalidFile(t *testing.T) {
 	filePath := "test1.txt"
-	expected := "0 0 0 test1.txt"
+	expected := "FILE READ ERROR"
 	actual := getFormattedOutput([]string{filePath})
+	if actual != expected {
+		t.Errorf("Expected %s, got %s", expected, actual)
+	}
+}
+
+func TestFormattedOutput_BytesFlag_ValidFile(t *testing.T) {
+	filePath := "test.txt"
+	expected := "342384 test.txt"
+	actual := getFormattedOutput([]string{"-c", filePath})
+	if actual != expected {
+		t.Errorf("Expected %s, got %s", expected, actual)
+	}
+}
+
+func TestFormattedOutput_BytesFlag_InvalidFile(t *testing.T) {
+	filePath := "test1.txt"
+	expected := "FILE READ ERROR"
+	actual := getFormattedOutput([]string{"-c", filePath})
+	if actual != expected {
+		t.Errorf("Expected %s, got %s", expected, actual)
+	}
+}
+
+func TestFormattedOutput_CharsFlag_ValidFile(t *testing.T) {
+	filePath := "test.txt"
+	expected := "339486 test.txt"
+	actual := getFormattedOutput([]string{"-m", filePath})
+	if actual != expected {
+		t.Errorf("Expected %s, got %s", expected, actual)
+	}
+}
+
+func TestFormattedOutput_CharsAndLinesFlag_ValidFile(t *testing.T) {
+	filePath := "test.txt"
+	expected := "7189 339486 test.txt"
+	actual := getFormattedOutput([]string{"-ml", filePath})
 	if actual != expected {
 		t.Errorf("Expected %s, got %s", expected, actual)
 	}
